c7fb167f8d835f7c4b942bdd58c9328ff8374f04
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2020-12-21 17:36:06 -0800

    redo the dupeti du

--- SympleCompiler/src/SympleCode/Emit/Emitter.cpp
+++ SympleCompiler/src/SympleCode/Emit/Emitter.cpp
@@ -5,29 +5,18 @@
 #include <cstring>
 #include <memory>
 
-#include "SympleCode/Common/Node/Member/FunctionHintNode.h"
-#include "SympleCode/Common/Node/Member/ExternFunctionNode.h"
-#include "SympleCode/Common/Node/Member/GlobalStatementNode.h"
-
-#include "SympleCode/Common/Node/Statement/ExpressionStatementNode.h"
-
-#include "SympleCode/Common/Node/Expression/PointerIndexExpressionNode.h"
-#include "SympleCode/Common/Node/Expression/ParenthesizedExpressionNode.h"
-#include "SympleCode/Common/Node/Expression/NumberLiteralExpressionNode.h"
-#include "SympleCode/Common/Node/Expression/BooleanLiteralExpressionNode.h"
-
-#if DO_COMMENTS
-#define Comment(fmt, ...) (void)fprintf_s(mFile, "# " fmt "\n", __VA_ARGS__)
-#else
-#define Comment(fmt, ...) (void())
-#endif
 #define Write(fmt, ...) ((void)fprintf_s(mFile, fmt "\n", __VA_ARGS__))
 #define WriteLiteral(fmt, ...) ((void)fprintf_s(mLiteralFile, fmt "\n", __VA_ARGS__))
+
+#define RegSp ("%esp")
+#define RegBp ("%ebp")
+
+#define FORMAT__MAX 128
 
 namespace Symple
 {
 	Emitter::Emitter(Diagnostics* diagnostics, const char* path)
-		: mDiagnostics(diagnostics), mPath(path), mStackPos(0), mStringPos(0), mJumpPos(0), mDeclaredVariables()
+		: mDiagnostics(diagnostics), mPath(path), mStackPos(0), mDataPos(0), mDeclaredVariables()
 	{
 		while (OpenFile());
 		while (OpenLiteralFile());
@@ -45,10 +34,15 @@ namespace Symple
 		fclose(mFile);
 	}
 
-	void Emitter::Emit(const CompilationUnitNode* unit)
+	char Emitter::Mod(int size)
 	{
-		for (const MemberNode* member : unit->GetMembers())
-			EmitMember(member);
+		if (size <= 1)
+			return 'b';
+		if (size <= 2)
+			return 'w';
+		if (size <= 4)
+			return 'l';
+		return ' ';
 	}
 
 	char* Emitter::RegAx(int size)
@@ -73,428 +67,34 @@ namespace Symple
 		return "%eax";
 	}
 
-	char* Emitter::RegCx()
+	char* Emitter::Format(char* fmt, ...)
 	{
-		return "%ecx";
+		char* out = new char[FORMAT__MAX];
+
+		va_list vl;
+		va_start(vl, fmt);
+		vsprintf_s(out, FORMAT__MAX, fmt, vl);
+		va_end(vl);
+
+		return out;
 	}
 
-	char* Emitter::RegSp()
+	void Emitter::Emit(const CompilationUnitNode* unit)
 	{
-		return "%esp";
+		for (const MemberNode* member : unit->GetMembers())
+			EmitMember(member);
 	}
 
-	char* Emitter::RegBp()
+	char* Emitter::EmitMember(const MemberNode* member)
 	{
-		return "%ebp";
-	}
-
-	std::string Emitter::EmitModifiableExpression(const ModifiableExpressionNode* expression, int size)
-	{
-		std::stringstream ss;
-
-		if (expression->Is<VariableExpressionNode>())
+		switch (member->GetKind())
 		{
-			ss << "_" << expression->Cast<VariableExpressionNode>()->GetName()->GetLex() << "(" << RegBp() << ")";
-		}
-		else if (expression->Is<PointerIndexExpressionNode>())
-		{
-			PointerIndexExpressionNode* expr = expression->Cast<PointerIndexExpressionNode>();
-
-			Write("\tpush%c    %s", Mod(), RegAx());
-			EmitExpression(expr->GetIndex());
-			Write("\tpush%c   %s", Mod(), RegAx());
-			Write("\tmov%c    _%s$(%s), %s", Mod(), std::string(expr->GetExpression()->GetName()->GetLex()).c_str(), RegBp(), RegAx());
-			Write("\tpop%c    %s", Mod(), RegDx());
-			Write("\tadd%c    %s, %s", Mod(), RegDx(), RegAx());
-			Write("\tpop%c    %s", Mod(), RegDx());
-			Write("\tmov%c    %s, (%s)", Mod(1), RegDx(1), RegAx());
-			Write("\tmov%c    %s, %s", Mod(1), RegDx(1), RegAx(1));
-			return EmitCast(1);
-			ss << "(" << RegAx() << ")";
-		}
-		else if (expression->Is<AssignmentExpressionNode>())
-		{
-			ss << EmitModifiableExpression(expression->Cast<AssignmentExpressionNode>()->GetLeft());
-		}
-
-		return ss.str();
-	}
-
-	char Emitter::Mod(int size)
-	{
-		if (size <= 1)
-			return 'b';
-		if (size <= 2)
-			return 'w';
-		if (size <= 4)
-			return 'l';
-		return ' ';
-	}
-
-	void Emitter::EmitCast(int from, int to)
-	{
-		if (from == to)
-			return;
-		return Write("\tmovs%c%c  %s, %s", Mod(from), Mod(to), RegAx(from), RegAx(to));
-	}
-
-	void Emitter::EmitMember(const MemberNode* member)
-	{
-		if (member->Is<FunctionDeclarationNode>())
+		case Node::Kind::FunctionHint:
 			return EmitFunctionDeclaration(member->Cast<FunctionDeclarationNode>());
-		if (member->Is<GlobalStatementNode>())
-			return EmitStatement(member->Cast<GlobalStatementNode>()->GetStatement());
-	}
-
-	void Emitter::EmitFunctionDeclaration(const FunctionDeclarationNode* declaration)
-	{
-		if (declaration->Is<FunctionHintNode>())
-			return;
-		if (declaration->Is<ExternFunctionNode>())
-			return Write(".extern _%s", std::string(declaration->GetName()->GetLex()).c_str());
-
-		unsigned int pStackPos = mStackPos;
-		mStackPos = 0;
-
-		std::string name = std::string(declaration->GetName()->GetLex());
-		Comment("Function Declaration");
-		if (name[0] != '$')
-			Write(".global _%s", name.c_str());
-		Write("_%s:", name.c_str());
-		Comment("\tPush Stack");
-		Write("\tpush    %s", RegBp());
-		Write("\tmovl    %s, %s", RegSp(), RegBp());
-		if (name == "main")
-			Write("\txorl    %%eax, %%eax");
-
-		Comment("\tFunction Arguments");
-		unsigned int pos = 4;
-		for (const FunctionArgumentNode* argument : declaration->GetArguments()->GetArguments())
-		{
-			pos += 4;
-			Write("_%s$ = %i", std::string(argument->GetName()->GetLex()).c_str(), pos);
-			mDeclaredVariables.insert({ argument->GetName()->GetLex(), new VariableDeclarationNode(argument->GetName(), argument->GetType(), nullptr) });
-		}
-
-		for (const StatementNode* statement : declaration->GetBody()->GetStatements())
-			EmitStatement(statement);
-
-		Comment("\tPop Stack and Return");
-		Write("\tmovl    %s, %s", RegBp(), RegSp());
-		Write("\tpop     %s", RegBp());
-		Write("\tret");
-
-		for (const auto& variable : declaration->GetBody()->GetVariables())
-			mDeclaredVariables.erase(mDeclaredVariables.find(variable.first));
-		for (const FunctionArgumentNode* argument : declaration->GetArguments()->GetArguments())
-			mDeclaredVariables.erase(mDeclaredVariables.find(argument->GetName()->GetLex()));
-
-		mStackPos = pStackPos;
-	}
-
-	void Emitter::EmitVariableDeclaration(const VariableDeclarationNode* declaration)
-	{
-		std::string name = std::string(declaration->GetName()->GetLex());
-		unsigned int size = declaration->GetType()->GetSize();
-		mStackPos += size;
-
-		mDeclaredVariables.insert({ declaration->GetName()->GetLex(), declaration });
-
-		Write("_%s$ = -%i", name.c_str(), mStackPos);
-		Write("\tsubl    $%i, %s", size, RegSp());
-		if (declaration->GetInitializer()->GetKind() != Node::Kind::Expression)
-		{
-			EmitExpression(declaration->GetInitializer(), size);
-			Write("\tmov%c    %s, _%s$(%s)", Mod(size), RegAx(size), name.c_str(), RegBp());
 		}
 	}
 
-	void Emitter::EmitStatement(const StatementNode* statement)
-	{
-		if (statement->Is<ExpressionStatementNode>())
-			return EmitExpression(statement->Cast<ExpressionStatementNode>()->GetExpression());
-		if (statement->Is<ReturnStatementNode>())
-			EmitExpression(statement->Cast<ReturnStatementNode>()->GetExpression());
-		if (statement->Is<IfStatementNode>())
-			return EmitIfStatement(statement->Cast<IfStatementNode>());
-		if (statement->Is<WhileStatementNode>())
-			return EmitWhileStatement(statement->Cast<WhileStatementNode>());
-		if (statement->Is<VariableDeclarationNode>())
-			return EmitVariableDeclaration(statement->Cast<VariableDeclarationNode>());
-		if (statement->Is<BlockStatementNode>())
-			return EmitBlockStatement(statement->Cast<BlockStatementNode>());
-		if (statement->Is<BreakStatementNode>())
-			return EmitBreakStatement(statement->Cast<BreakStatementNode>());
-	}
-
-	void Emitter::EmitIfStatement(const IfStatementNode* statement)
-	{
-		unsigned int afterJumpPos = mJumpPos;
-		mJumpPos++;
-		unsigned int elseJumpPos = mJumpPos;
-		mJumpPos++;
-
-
-		EmitExpression(statement->GetCondition());
-		Write("\tcmp%c    $0, %s", Mod(), RegAx());
-		Write("\tje      ..Jump.%i", elseJumpPos);
-
-		for (const StatementNode* statement : statement->GetThen()->GetStatements())
-			EmitStatement(statement);
-
-		Write("\tjmp     ..Jump.%i", afterJumpPos);
-		Write("..Jump.%i:", elseJumpPos);
-
-		for (const StatementNode* statement : statement->GetElse()->GetStatements())
-			EmitStatement(statement);
-
-		Write("..Jump.%i:", afterJumpPos);
-	}
-
-	void Emitter::EmitBreakStatement(const BreakStatementNode* statement)
-	{
-		if (!mJumpPos)
-			return mDiagnostics->ReportError(statement->GetStatement(), "There is Nothing to Break");
-		Write("\tjmp     ..Jump.%i", mJumpPos - 1);
-	}
-
-	void Emitter::EmitBlockStatement(const BlockStatementNode* statement)
-	{
-		for (const StatementNode* statement : statement->GetStatements())
-			EmitStatement(statement);
-
-		for (const auto& variable : statement->GetVariables())
-			mDeclaredVariables.erase(mDeclaredVariables.find(variable.first));
-	}
-
-	void Emitter::EmitWhileStatement(const WhileStatementNode* statement)
-	{
-		unsigned int pJumpPos = mJumpPos;
-		mJumpPos++;
-		unsigned int aJumpPos = mJumpPos;
-		mJumpPos++;
-
-		Write("..Jump.%i:", pJumpPos);
-
-		EmitExpression(statement->GetCondition());
-		Write("\tcmp%c    $0, %s", Mod(), RegAx());
-		Write("\tje      ..Jump.%i", aJumpPos);
-
-		for (const StatementNode* statement : statement->GetBody()->GetStatements())
-			EmitStatement(statement);
-
-		Write("\tjmp     ..Jump.%i", pJumpPos);
-		Write("..Jump.%i:", aJumpPos);
-	}
-
-	void Emitter::EmitExpression(const ExpressionNode* expression, int size)
-	{
-		Write("");
-		if (expression->Is<ParenthesizedExpressionNode>())
-			return EmitExpression(expression->Cast<ParenthesizedExpressionNode>()->GetExpression(), size);
-		if (expression->Is<UnaryExpressionNode>())
-			return EmitUnaryExpression(expression->Cast<UnaryExpressionNode>(), size);
-		if (expression->Is<BinaryExpressionNode>())
-			return EmitBinaryExpression(expression->Cast<BinaryExpressionNode>(), size);
-		if (expression->Is<FunctionCallExpressionNode>())
-			return EmitFunctionCallExpression(expression->Cast<FunctionCallExpressionNode>(), size);
-		if (expression->Is<LiteralExpressionNode>())
-			return EmitLiteralExpression(expression->Cast<LiteralExpressionNode>(), size);
-		if (expression->Is<VariableExpressionNode>())
-			return EmitVariableExpression(expression->Cast<VariableExpressionNode>(), size);
-		if (expression->Is<PointerIndexExpressionNode>())
-			return EmitPointerIndexExpression(expression->Cast<PointerIndexExpressionNode>(), size);
-	}
-
-	void Emitter::EmitLiteralExpression(const LiteralExpressionNode* expression, int size)
-	{
-		if (expression->Is<StringLiteralExpressionNode>())
-			return EmitStringLiteralExpression(expression->Cast<StringLiteralExpressionNode>());
-
-		if (expression->Is<NumberLiteralExpressionNode>())
-			Write("\tmov%c    $%s, %s", Mod(size), std::string(expression->GetLiteral()->GetLex()).c_str(), RegAx(size));
-		if (expression->Is<BooleanLiteralExpressionNode>())
-			Write("\tmov%c    $%i, %s", Mod(size), expression->GetLiteral()->Is(Token::Kind::True), RegAx(size));
-	}
-
-	void Emitter::EmitUnaryExpression(const UnaryExpressionNode* expression, int size)
-	{
-		Comment("Unary Expression [%s]", std::string(expression->GetOperator()->GetLex()).c_str());
-		if (expression->GetOperator()->Is(Token::Kind::At))
-		{
-			if (expression->GetValue()->GetKind() == Node::Kind::VariableExpression)
-			{
-				std::string name(expression->GetValue()->Cast<VariableExpressionNode>()->GetName()->GetLex());
-				return Write("\tlea%c    _%s$(%s), %s", Mod(mDeclaredVariables[name]->GetType()->GetSize()), name.c_str(), RegBp(), RegAx());
-			}
-			if (expression->GetValue()->GetKind() == Node::Kind::PointerIndexExpression)
-			{
-				const PointerIndexExpressionNode* expr = expression->GetValue()->Cast<PointerIndexExpressionNode>();
-				std::string name(expr->GetExpression()->GetName()->GetLex());
-
-				EmitExpression(expr->GetIndex());
-				Write("\tmov%c    %s, %s", Mod(), RegAx(), RegDx());
-				Write("\tlea%c    _%s$(%s), %s", Mod(mDeclaredVariables[name]->GetType()->GetSize()), name.c_str(), RegBp(), RegAx());
-				return Write("\tadd%c    %s, %s", Mod(), RegDx(), RegAx());
-			}
-
-			return mDiagnostics->ReportError(expression->GetOperator(), "Expected lvalue");
-		}
-		EmitExpression(expression->GetValue());
-
-		switch (expression->GetOperator()->GetKind())
-		{
-		case Token::Kind::Asterisk:
-			Write("\tmov%c    (%s), %s", Mod(), RegAx(), RegAx());
-			break;
-		case Token::Kind::Minus:
-			Write("\tneg%c    %s", Mod(), RegAx());
-			break;
-		}
-	}
-
-	void Emitter::EmitBinaryExpression(const BinaryExpressionNode* expression, int size)
-	{
-		Comment("Binary Expression [%s]", std::string(expression->GetOperator()->GetLex()).c_str());
-		if (expression->GetOperator()->Is(Token::Kind::Equal))
-		{
-			if (expression->GetLeft()->GetKind() == Node::Kind::VariableExpression)
-			{
-				EmitExpression(expression->GetRight(), size);
-				return Write("\tmov%c    %s, _%s$(%s)", Mod(), RegAx(), std::string(expression->GetLeft()->Cast<VariableExpressionNode>()->GetName()->GetLex()).c_str(), RegBp());
-			}
-			if (expression->GetLeft()->GetKind() == Node::Kind::PointerIndexExpression)
-			{
-				const PointerIndexExpressionNode* expr = expression->GetLeft()->Cast<PointerIndexExpressionNode>();
-
-				EmitExpression(expression->GetRight());
-				Write("\tpush%c    %s", Mod(), RegAx());
-				EmitExpression(expr->GetIndex());
-				Write("\tpush%c   %s", Mod(), RegAx());
-				Write("\tmov%c    _%s$(%s), %s", Mod(), std::string(expr->GetExpression()->GetName()->GetLex()).c_str(), RegBp(), RegAx());
-				Write("\tpop%c    %s", Mod(), RegDx());
-				Write("\tadd%c    %s, %s", Mod(), RegDx(), RegAx());
-				Write("\tpop%c    %s", Mod(), RegDx());
-				Write("\tmov%c    %s, (%s)", Mod(1), RegDx(1), RegAx());
-				Write("\tmov%c    %s, %s", Mod(1), RegDx(1), RegAx(1));
-				return EmitCast(1);
-			}
-			
-			return mDiagnostics->ReportError(expression->GetOperator(), "Expected lvalue");
-		}
-
-		EmitExpression(expression->GetRight());
-		Write("\tpush%c   %s", Mod(), RegAx());
-		EmitExpression(expression->GetLeft());
-		Write("\tpop%c    %s", Mod(), RegDx());
-
-		switch (expression->GetOperator()->GetKind())
-		{
-		case Token::Kind::Plus:
-			Write("\tadd%c    %s, %s", Mod(), RegDx(), RegAx());
-			break;
-		case Token::Kind::Minus:
-			Write("\tsub%c    %s, %s", Mod(), RegDx(), RegAx());
-			break;
-		case Token::Kind::Asterisk:
-			Write("\timu%c    %s, %s", Mod(), RegDx(), RegAx());
-			break;
-		case Token::Kind::EqualEqual:
-		{
-			unsigned int jumpPos = mJumpPos;
-			mJumpPos++;
-
-			Write("\tcmp%c    %s, %s", Mod(), RegDx(), RegAx());
-			Write("\tmov%c    $1, %s", Mod(), RegAx());
-			Write("\tje      ..Jump.%i", jumpPos);
-			Write("\tmov%c    $0, %s", Mod(), RegAx());
-			Write("..Jump.%i:", jumpPos);
-			break;
-		}
-		case Token::Kind::ExclamationEqual:
-		{
-			unsigned int jumpPos = mJumpPos;
-			mJumpPos++;
-
-			Write("\tcmp%c    %s, %s", Mod(), RegDx(), RegAx());
-			Write("\tmov%c    $0, %s", Mod(), RegAx());
-			Write("\tje      ..Jump.%i", jumpPos);
-			Write("\tmov%c    $1, %s", Mod(), RegAx());
-			Write("..Jump.%i:", jumpPos);
-			break;
-		}
-		}
-	}
-
-	void Emitter::EmitVariableExpression(const VariableExpressionNode* expression, int size)
-	{
-		std::string name = std::string(expression->GetName()->GetLex());
-		int varSize = mDeclaredVariables[name]->GetType()->GetSize();
-		if (!VariableDefined(expression->GetName()->GetLex()))
-			return mDiagnostics->ReportError(expression->GetName(), "'%s' is not a Variable", name.c_str());
-
-		Write("\tmov%c    _%s$(%s), %s", Mod(varSize), name.c_str(), RegBp(), RegAx(varSize));
-		EmitCast(varSize);
-	}
-
-	void Emitter::EmitFunctionCallExpression(const FunctionCallExpressionNode* call, int size)
-	{
-		const FunctionDeclarationNode* function = mDiagnostics->GetFunction(call->GetName()->GetLex());
-		if (!function)
-			return mDiagnostics->ReportError(call->GetName(), "Function does not exist!");
-
-		Comment("Function Call");
-		Comment("\tPush Arguments");
-
-		unsigned int pushedSize = 0;
-		for (int i = call->GetArguments()->GetArguments().size() - 1; i >= 0; i--)
-		{
-			EmitExpression(call->GetArguments()->GetArguments()[i]);
-			Write("\tpush%c   %s", Mod(), RegAx());
-
-			pushedSize += 4;
-		}
-		Comment("\tCall Function");
-		Write("\tcall    _%s", std::string(call->GetName()->GetLex()).c_str());
-		Comment("\tPop Arguments");
-		Write("\taddl    $%i, %s", pushedSize, RegSp());
-	}
-
-	void Emitter::EmitAssignmentExpression(const AssignmentExpressionNode* expression, int size = 4)
-	{
-		switch (expression->GetOperator()->GetKind())
-		{
-		case Token::Kind::Equal:
-			Write("mov%c %s, %s");
-			break;
-		}
-
-		Write("mov%c %s, %s", Mod(), RegAx(), ModifiableExpression(expression).c_str());
-	}
-
-	void Emitter::EmitPointerIndexExpression(const PointerIndexExpressionNode* expression, int size)
-	{
-		EmitExpression(expression->GetIndex());
-		Write("\tmov%c    %s, %s", Mod(), RegAx(), RegDx());
-		Write("\tmov%c    _%s$(%s), %s", Mod(), std::string(expression->GetExpression()->GetName()->GetLex()).c_str(), RegBp(), RegAx());
-		Write("\tadd%c    %s, %s", Mod(), RegDx(), RegAx());
-		Write("\tmov%c    (%s), %s", Mod(), RegAx(), RegAx());
-	}
-
-	void Emitter::EmitStringLiteralExpression(const StringLiteralExpressionNode* literal, int size)
-	{
-		WriteLiteral("..String.%i:", mStringPos);
-		WriteLiteral("\t.string \"%s\"", std::string(literal->GetLiteral()->GetLex()).c_str());
-		Write("\tmov%c    $..String.%i, %s", Mod(), mStringPos, RegAx());
-
-		mStringPos++;
-	}
-
-	bool Emitter::VariableDefined(const std::string_view& name)
-	{
-		return mDeclaredVariables.find(name) != mDeclaredVariables.end();
-	}
-
+	#pragma region File Stuff
 	bool Emitter::OpenFile()
 	{
 		errno_t err = fopen_s(&mFile, mPath, "w");
@@ -574,4 +174,5 @@ namespace Symple
 
 		return true;
 	}
+	#pragma endregion
 }
