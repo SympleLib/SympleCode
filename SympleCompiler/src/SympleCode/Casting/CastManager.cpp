bfa41b0ab96f323152d847466069991f41d589ab
Treidex <telkids4@gmail.com>
2021-01-04 18:04:47 -0800

    hmmm 🤔

--- SympleCompiler/src/SympleCode/Casting/CastManager.cpp
+++ SympleCompiler/src/SympleCode/Casting/CastManager.cpp
@@ -1,5 +1,7 @@
 #include "SympleCode/Casting/CastManager.h"
 
+#include <algorithm>
+
 namespace Symple
 {
 	std::map<const Type*, const Type*> CastManager::sTypeCastSheet = {
@@ -21,9 +23,41 @@ namespace Symple
 		{ Token::Kind::Mutable, Token::Kind::Unknown },
 	};
 
+	bool CastManager::CanCast(const TypeModifiersNode* from, const TypeModifiersNode* to)
+	{
+		bool can = true;
+		for (unsigned int i = 0; i < from->GetModifiers().size(); i++)
+		{
+			bool thisCan = false;
+
+			for (unsigned int j = 0; j < to->GetModifiers().size(); j++)
+			{
+				Token::Kind fromKind = Token::Kind::Unknown;
+				Token::Kind toKind = Token::Kind::Unknown;
+
+				if (j < from->GetModifiers().size())
+					fromKind = from->GetModifiers()[i]->GetModifier()->GetKind();
+				if (i < to->GetModifiers().size())
+					toKind = to->GetModifiers()[i]->GetModifier()->GetKind();
+
+				if (thisCan |= CanCast(fromKind, toKind))
+					break;
+			}
+
+			can &= thisCan;
+		}
+
+		return can;
+	}
+
+	bool CastManager::CanCast(const TypeModifierNode* from, const TypeModifierNode* to)
+	{
+		return CanCast(from->GetModifier()->GetKind(), to->GetModifier()->GetKind());
+	}
+
 	bool CastManager::CanCast(Token::Kind from, Token::Kind to)
 	{
-
+		return sModifierCastSheet.find(from) != sModifierCastSheet.end();
 	}
 
 	bool CastManager::CanCast(const Type* from, const Type* to)
--- SympleCompiler/src/SympleCode/Casting/CastManager.h
+++ SympleCompiler/src/SympleCode/Casting/CastManager.h
@@ -14,8 +14,12 @@ namespace Symple
 		static std::map<const Type*, const Type*> sTypeCastSheet;
 		static std::map<Token::Kind, Token::Kind> sModifierCastSheet;
 	public:
+		static bool CanCast(const TypeModifiersNode* from, const TypeModifiersNode* to);
+		static bool CanCast(const TypeModifierNode* from, const TypeModifierNode* to);
 		static bool CanCast(Token::Kind from, Token::Kind to);
+
 		static bool CanCast(const Type* from, const Type* to);
+
 		static bool CanCast(const TypeNode* from, const TypeNode* to);
 	};
 }
