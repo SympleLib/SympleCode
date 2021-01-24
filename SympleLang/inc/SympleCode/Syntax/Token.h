a4a20530ebc91184955d0fc2412b99ce08946230
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2021-01-23 18:25:59 -0800

    Token

---
+++ SympleLang/inc/SympleCode/Syntax/Token.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string_view>
+
+namespace Symple
+{
+	class Token
+	{
+	public: enum Kind;
+	private:
+		Kind mKind;
+		std::string_view mText;
+
+		char* mFile;
+		unsigned mLine, mColumn;
+	public:
+		Token(Kind kind = Unknown, unsigned ln = 0, unsigned col = 0, char* file = "<NA>");
+		Token(Kind kind, std::string_view text, unsigned ln = 0, unsigned col = 0, char* file = "<NA>");
+		Token(Kind kind, char* beg, unsigned len = 1, unsigned ln = 0, unsigned col = 0, char* file = "<NA>");
+		Token(Kind kind, char* beg, char* end, unsigned ln = 0, unsigned col = 0, char* file = "<NA>");
+
+		bool Is(Kind kind);
+		template <typename... Args>
+		bool IsEither(Kind kind, Args... kinds);
+
+		Kind GetKind();
+		std::string_view GetText();
+
+		char* GetFile();
+		unsigned GetLine();
+		unsigned GetColumn();
+	public:
+		enum Kind
+		{
+			EndOfFile,
+				Unknown,
+
+				Identifier,
+
+				Last = Identifier,
+		};
+
+		static constexpr char* KindMap[(int)Last + 1] = {
+			"EndOfFile",
+			"Unknown",
+
+			"Identifier",
+		};
+	};
+}
---
+++ SympleLang/src/Syntax/Token.cpp
@@ -0,0 +1,50 @@
+#include "SympleCode/Syntax/Token.h"
+
+namespace Symple
+{
+	Token::Token(Kind kind, unsigned ln, unsigned col, char* file)
+		: mKind(kind), mText(), mLine(ln), mColumn(col), mFile(file)
+	{}
+
+	Token::Token(Kind kind, std::string_view text, unsigned ln, unsigned col, char* file)
+		: mKind(kind), mText(text), mLine(ln), mColumn(col), mFile(file)
+	{}
+
+	Token::Token(Kind kind, char* beg, unsigned len, unsigned ln, unsigned col, char* file)
+		: mKind(kind), mText(beg, len), mLine(ln), mColumn(col), mFile(file)
+	{}
+
+	Token::Token(Kind kind, char* beg, char* end, unsigned ln, unsigned col, char* file)
+		: mKind(kind), mText(beg, std::distance(beg, end)), mLine(ln), mColumn(col), mFile(file)
+	{}
+
+
+	bool Token::Is(Kind kind)
+	{
+		return mKind == kind;
+	}
+
+	template <typename... Args>
+	bool Token::IsEither(Kind kind, Args... kinds)
+	{
+		return Is(kind) || IsEither(kinds...);
+	}
+
+
+	Token::Kind Token::GetKind()
+	{ return mKind; }
+
+	std::string_view Token::GetText()
+	{ return mText; }
+
+
+	char* Token::GetFile()
+	{ return mFile; }
+
+	unsigned Token::GetLine()
+	{ return mLine; }
+
+	unsigned Token::GetColumn()
+	{ return mColumn; }
+
+}
