5fcc0d94faab42db9f75b61d019fa9e6fa7cbf94
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2021-01-26 20:14:29 -0800

    diner

--- SympleLang/src/DiagnosticBag.cpp
+++ SympleLang/src/DiagnosticBag.cpp
@@ -2,8 +2,12 @@
 
 namespace Symple
 {
-	DiagnosticBag::ReportMessage(std::shared_ptr<Syntax::Token> tok, std::string& )
-	{
+	void DiagnosticBag::ReportMessage(std::shared_ptr<Syntax::Token> tok, std::string_view msg)
+	{ mDiagnostics.push_back(make_shared<Diagnostic>(Diagnostic::Message, tok, msg)); }
 
-	}
+	void DiagnosticBag::ReportWarning(std::shared_ptr<Syntax::Token> tok, std::string_view msg)
+	{ mDiagnostics.push_back(make_shared<Diagnostic>(Diagnostic::Warning, tok, msg)); }
+
+	void DiagnosticBag::ReportError(std::shared_ptr<Syntax::Token> tok, std::string_view msg)
+	{ mDiagnostics.push_back(make_shared<Diagnostic>(Diagnostic::Error, tok, msg)); }
 }
