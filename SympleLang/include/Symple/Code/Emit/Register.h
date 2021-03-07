610af3f8867eb7d86687b2456574c64813c39d97
Treidex <treidexsemextetry@gmail.com>
2021-03-06 17:34:36 -0800

    registers

---
+++ SympleLang/include/Symple/Code/Emit/Register.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Symple/Code/Type/Type.h"
+
+namespace Symple::Code
+{
+	enum class SYC_API RegKind
+	{
+		Ax,
+		Bx,
+		Cx,
+		Dx,
+
+		Sp,
+		Bp,
+
+		Count, // Count of register kinds
+		LastX = Dx, // Last `free` register
+	};
+
+	constexpr const char *const RegKindNames[(uint32)RegKind::Count] =
+	{
+		"Ax",
+		"Bx",
+		"Cx",
+		"Dx",
+
+		"Sp",
+		"Bp",
+	};
+
+	SYC_API std::ostream &operator <<(std::ostream &os, RegKind);
+
+	class Register: public Printable
+	{
+	public:
+		using Type_t = Type;
+	private:
+		RegKind m_Kind;
+		GlobalRef<const Type_t> m_Type;
+	public:
+		Register(RegKind, GlobalRef<const Type_t> type);
+		virtual void Print(std::ostream &, std::string indent = "", std::string_view label = "", bool last = true) const override;
+
+		RegKind GetKind() const;
+		GlobalRef<const Type_t> GetType() const;
+
+		SY_PROPERTY_GET(GetKind) RegKind Kind;
+		SY_PROPERTY_GET(GetType) Type_t Type;
+	};
+}
