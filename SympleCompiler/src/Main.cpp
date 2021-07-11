347ee34e99e9f85dea1e6b285f2bf238c944f995
Treidex <treidexsemextetry@gmail.com>
2021-07-11 14:03:44 -0700

    maybe cs?

--- SympleCompiler/src/Main.cpp
+++ SympleCompiler/src/Main.cpp
@@ -1,7 +1,12 @@
 #include "Symple/Code/Util/Console.h"
+#include "Symple/Code/Util/File.h"
 
+using namespace Symple::Code;
 int main()
 {
-	puts("Wassup!");
+	Console.color = ConsoleColor::Green;
+
+	File file("sy/Main.sy", FilePermissions::Read);
+	puts(file.GetSource().c_str());
 	(void)getchar();
 }
