0b5d38798f16306826b91f636e4239045cdabaa5
Treidex <treidexsemextetry@gmail.com>
2021-07-11 15:04:48 -0700

    Bamm, syc done

--- SuperCode/Program.cs
+++ SuperCode/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using LLVMSharp.Interop;
 
 namespace SuperCode
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Wazzyp!");
+			LLVMModuleRef module = LLVMModuleRef.CreateWithName("SympleCode");
+			LLVMBuilderRef builder = LLVMBuilderRef.Create(module.Context);
+			
+			LLVMTypeRef[] paramz = new LLVMTypeRef[2] { LLVMTypeRef.Int32, LLVMTypeRef.Int32 };
+			LLVMTypeRef ft = LLVMTypeRef.CreateFunction(LLVMTypeRef.Int32, paramz);
+			LLVMValueRef fn = module.AddFunction("Add", ft);
+
+			LLVMBasicBlockRef entry = fn.AppendBasicBlock("Entry");
+			builder.PositionAtEnd(entry);
+			LLVMValueRef ret = builder.BuildAdd(fn.Params[0], fn.Params[1]);
+			builder.BuildRet(ret);
+
+			
+			Console.WriteLine(fn.PrintToString());
 			Console.ReadKey();
 		}
 	}
