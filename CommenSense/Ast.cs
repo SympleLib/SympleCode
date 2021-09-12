4d7e0edb7044552c399c15ef14e4e1ec8406d150
Treidex <treidexwastaken@gmail.com>
2021-09-12 12:49:39 -0700

    yes, flattering ✨✨

--- CommenSense/Ast.cs
+++ CommenSense/Ast.cs
@@ -1,36 +1,36 @@
 ﻿namespace CommenSense;
 
 record Ast;
-record ModuleAst(string name, StmtAst[] members);
+partial record ModuleAst(string name, StmtAst[] members);
 
-record TypeAst(string typeBase, int ptrCount): Ast;
-record ParamAst(TypeAst type, string name, ExprAst defaultExpr): Ast;
-record FieldAst(Enum vis, TypeAst type, string name, ExprAst initializer): Ast;
+partial record TypeAst(string typeBase, int ptrCount): Ast;
+partial record ParamAst(TypeAst type, string name, ExprAst defaultExpr): Ast;
+partial record FieldAst(Enum vis, TypeAst type, string name, ExprAst initializer): Ast;
 
 
 record StmtAst: Ast;
 
-record ImplAst(Enum vis, string name, FuncAst[] funcs): StmtAst;
-record StructAst(Enum vis, string name, FieldAst[] fields): StmtAst;
-record ClassAst(Enum vis, string name, FieldAst[] fields, FuncAst[] funcs): StmtAst;
+partial record ImplAst(Enum vis, string name, FuncAst[] funcs): StmtAst;
+partial record StructAst(Enum vis, string name, FieldAst[] fields): StmtAst;
+partial record ClassAst(Enum vis, string name, FieldAst[] fields, FuncAst[] funcs): StmtAst;
 
-record DeclFuncAst(Enum vis, TypeAst retType, string name, ParamAst[] paramz): StmtAst;
-record FuncAst(Enum vis, TypeAst retType, string name, ParamAst[] paramz, StmtAst[] body): StmtAst;
-record DeclVarAst(Enum vis, TypeAst type, string name): StmtAst;
-record VarAst(Enum vis, TypeAst type, string name, ExprAst initializer): StmtAst;
+partial record DeclFuncAst(Enum vis, TypeAst retType, string name, ParamAst[] paramz): StmtAst;
+partial record FuncAst(Enum vis, TypeAst retType, string name, ParamAst[] paramz, StmtAst[] body): StmtAst;
+partial record DeclVarAst(Enum vis, TypeAst type, string name): StmtAst;
+partial record VarAst(Enum vis, TypeAst type, string name, ExprAst initializer): StmtAst;
 
-record ExprStmtAst(ExprAst expr): StmtAst;
+partial record ExprStmtAst(ExprAst expr): StmtAst;
 
 
 record ExprAst: Ast;
 
-record LiteralExprAst(): ExprAst;
-record IntLiteralExprAst(ulong value): LiteralExprAst;
-record StrLiteralExprAst(string value): LiteralExprAst;
-record FloatLiteralExprAst(double value): LiteralExprAst;
+partial record LiteralExprAst(): ExprAst;
+partial record IntLiteralExprAst(ulong value): LiteralExprAst;
+partial record StrLiteralExprAst(string value): LiteralExprAst;
+partial record FloatLiteralExprAst(double value): LiteralExprAst;
 
-record VarExprAst(string varName): ExprAst;
-record FuncPtrAst(string funcName, TypeAst[] paramTypes): ExprAst;
+partial record VarExprAst(string varName): ExprAst;
+partial record FuncPtrAst(string funcName, TypeAst[] paramTypes): ExprAst;
 
-record UnExprAst(Enum op, ExprAst operand): ExprAst;
-record BiExprAst(Enum op, ExprAst left, ExprAst right): ExprAst;
+partial record UnExprAst(Enum op, ExprAst operand): ExprAst;
+partial record BiExprAst(Enum op, ExprAst left, ExprAst right): ExprAst;
---
+++ CommenSense/Util.cs
@@ -0,0 +1,35 @@
+﻿namespace CommenSense;
+
+partial record ModuleAst
+{ public override string ToString() => $"module `{name}`: [\n{string.Join<StmtAst>(",\n", members)}\n]"; }
+
+
+partial record TypeAst
+{ public override string ToString() => $"{typeBase}{new string('*', ptrCount)}"; }
+
+partial record ParamAst
+{ public override string ToString() => $"[param] {type} {name} = {defaultExpr}"; }
+
+partial record FieldAst
+{ public override string ToString() => $"[field] {vis} {type} {name} = {initializer}"; }
+
+
+partial record ExprStmtAst
+{ public override string ToString() => expr.ToString(); }
+
+
+partial record IntLiteralExprAst
+{ public override string ToString() => $"{{ {value} }}"; }
+
+partial record StrLiteralExprAst
+{ public override string ToString() => $"{{ `{value}` }}"; }
+
+partial record FloatLiteralExprAst
+{ public override string ToString() => $"{{ {value} }}"; }
+
+
+partial record UnExprAst
+{ public override string ToString() => $"{{ {op} {operand} }}"; }
+
+partial record BiExprAst
+{ public override string ToString() => $"{{ {left} {op} {right} }}"; }
