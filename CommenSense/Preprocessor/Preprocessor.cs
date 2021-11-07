9c9ee831cdc9be858a85f417ce21f2066bca2e1f
Treidex <treidexwastaken@gmail.com>
2021-11-06 19:23:24 -0700

    cya

---
+++ CommenSense/Preprocessor/Preprocessor.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace CommenSense;
+
+class Preprocessor
+{
+	readonly string folder;
+	readonly string filename;
+
+	public Preprocessor(string source, string filename)
+	{
+		this.filename = filename;
+		folder = Path.GetFileName(Path.GetDirectoryName(this.filename)!);
+	}
