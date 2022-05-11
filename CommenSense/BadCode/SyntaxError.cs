12d5e8eceb83596df156612ab9d1ca74c40c8105
Treidex <treidexwastaken@gmail.com>
2022-05-11 15:42:47 -0700

    :)

--- CommenSense/BadCode/SyntaxError.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CommenSense;
-
-public record SyntaxError(string msg, Token token)
-{
-	public override string ToString() =>
-		$"line {token.line}, col {token.col}: {msg}";
-}
--- CommenSense/Parser/StaticParser.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CommenSense;
-
-partial class Parser
-{
-	public static readonly Dictionary<string, Parser> parsers = new Dictionary<string, Parser>();
-}
