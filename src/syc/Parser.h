e5867ff0de3c7d798f49068d55105ca03cd8c6ba
Treidex <treidexwastaken@gmail.com>
2022-05-12 16:40:37 -0700

    parser

---
+++ src/syc/Parser.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022-present, Treidex <treidexy@gmail.com>
+ *
+ * MIT License
+ */
+
+#pragma once
+
+#include <vector>
+#include "syc/Token.h"
+#include "syc/ast/Ast.h"
+
+namespace syc {
+	class Parser {
+	public:
+		std::vector<Token> tokens;
+		size_t pos;
+
+		std::vector<AstNode *> ast;
+
+	public:
+		Parser() = default;
+
+		std::vector<AstNode *> Parse(const std::vector<Token> &tokens);
+
+	private:
+		ExprAst *ParseExpr();
+		ExprAst *ParseBinExpr(ExprAst *left = nullptr);
+		LiteralAst *ParseLiteral();
+
+		static BinaryOperator getBinaryOperator(TokenKind kind);
+
+		Token next();
+		Token peek(ssize_t offset = 0) const;
+	};
+} // syc
